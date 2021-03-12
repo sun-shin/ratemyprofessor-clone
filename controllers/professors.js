695e7f318ecb99a2a5658d955a157b29d0e46da0
sun <sunhanshin2@gmail.com>
2021-03-12 15:05:27 -0600

    implement dotenv and hide credentials

--- controllers/professors.js
+++ controllers/professors.js
@@ -1,10 +1,14 @@
-const Pool = require("pg").Pool;
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  user: "sshin2",
-  host: "localhost",
-  database: "ratemyprofessor",
-  password: "password",
-  port: 5432,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 });
 
 // Professors CRUD
@@ -83,6 +87,7 @@ const deleteProfessor = (req, res) => {
 };
 
 module.exports = {
+  pool,
   getProfessors,
   getProfessorById,
   createProfessor,
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,15 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "sshin2",
-  host: "localhost",
-  database: "ratemyprofessor",
-  password: "password",
-  port: 5432,
-});
+require("dotenv").config();
 
+const { Pool } = require("pg");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
 // Reviews CRUD
 
 // Reviews Index
@@ -77,6 +80,7 @@ const deleteReview = (request, response) => {
 };
 
 module.exports = {
+  pool,
   getReviews,
   getReviewById,
   createReview,
